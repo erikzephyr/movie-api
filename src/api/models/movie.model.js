4b293efb1ef156126719620dfd1388719503b193
erikzephyr <erikbv99421@gmail.com>
2018-11-12 00:35:17 +0000

    post works

---
+++ src/api/models/movie.model.js
@@ -0,0 +1,7 @@
+class Movie {
+  constructor(id, name) {
+    this.id = id;
+    this.name = name;
+  }
+}
+module.exports = Movie
--- src/server.js
+++ src/server.js
@@ -1,23 +1,45 @@
-const { port, env } = require('./config/vars');
-var express    = require('express');        // call express
+/**
+ * EXTERNAL
+ */
 const app = require('./config/express');
+var express    = require('express');
+const _ = require('lodash');
+
+/**
+ * LOCAL
+ */
+const { port, env } = require('./config/vars');
+var Movie = require('./api/models/movie.model');
+
 
 let users = [{id: "1", name: "Name 1"}, {id: "2", name: "Name 2"}]
 
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();              // get an instance of the express Router
+/*
+* ROUTES FOR OUR API
+*/
+var router = express.Router();
 
+router.use(function(req, res, next) {
+  console.log('Something is happening.');
+  next();
+});
 
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
 });
 
+router.route('/movies/:name').post(function(req, res) {
+  var movie = new Movie(_.random(0,100), req.params.name);
+  res.json({
+    message: 'Movie created!',
+    movie: {
+      id: movie.id,
+      name: movie.name
+    }
+  });
 
-
+});
 app.use('/api', router);
-
-
 app.listen(port, () => console.info(`server started on port ${port} (${env})`));
 
 /**
