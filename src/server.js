7570b58a24d8ab130043cc620fb1de0967dbd2ec
erikzephyr <erikbv99421@gmail.com>
2018-11-12 00:00:27 +0000

    express works

--- src/server.js
+++ src/server.js
@@ -1,13 +1,25 @@
 const { port, env } = require('./config/vars');
+var express    = require('express');        // call express
 const app = require('./config/express');
 
 let users = [{id: "1", name: "Name 1"}, {id: "2", name: "Name 2"}]
 
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
+
+
+
+app.use('/api', router);
+
+
 app.listen(port, () => console.info(`server started on port ${port} (${env})`));
 
-app.get('/users/all', (req, res) => {
-  res.send(users);
-});
 /**
 * Exports express
 * @public
